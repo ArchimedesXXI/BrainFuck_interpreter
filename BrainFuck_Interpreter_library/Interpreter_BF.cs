c6e7f2f8efa095e944eba83b84e764834f2a99c8
MarcinBukowski <bukowski_marcin@wp.pl>
2017-06-04 21:13:20 +0200

    Added all source code files.

---
+++ BrainFuck_Interpreter_library/Interpreter_BF.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainFuck_Interpreter_library
+{
+    public class Interpreter_BF
+    {
+        #region Constructors
+        public Interpreter_BF(string BF_machine_instructions, string Inputs)
+        {
+            this.memoryPointer = 0;
+            this.instructionPointer = 0;
+            this.inputPointer = 0;
+            this.machine_memory = new byte[ushort.MaxValue + 1];
+            this.BF_machine_instructions = BF_machine_instructions;
+            this.Inputs = Inputs;
+            this.loop_call_stack = new Stack<int>();
+            this.Outputs = "";
+        }
+        #endregion
+
+        #region Fields
+        private ushort memoryPointer;
+        private int instructionPointer;
+        private int inputPointer;
+        private byte[] machine_memory;
+        private string BF_machine_instructions;
+        private string Inputs;
+        private Stack<int> loop_call_stack;
+        #endregion
+
+        #region Properties
+        public string Outputs { get; private set; }
+        #endregion
+
+        #region Methods
+        public void Interpret()
+        {
+            StringBuilder OutputBuilder = new StringBuilder();
+
+            while (this.instructionPointer < this.BF_machine_instructions.Length)
+            {
+                char ch = this.BF_machine_instructions[this.instructionPointer];
+
+                switch (ch)
+                {
+                    case '>':
+                        this.memoryPointer++;
+                        goto default;
+
+                    case '<':
+                        this.memoryPointer--;
+                        goto default;
+
+                    case '+':
+                        this.machine_memory[this.memoryPointer]++;
+                        goto default;
+
+                    case '-':
+                        this.machine_memory[this.memoryPointer]--;
+                        goto default;
+
+                    case '.':
+                        OutputBuilder.Append((char)this.machine_memory[this.memoryPointer]);
+                        goto default;
+
+                    case ',':
+                        this.machine_memory[this.memoryPointer] = (byte)this.Inputs[this.inputPointer];
+                        this.inputPointer++;
+                        goto default;
+
+                    case '[':
+                        if (this.machine_memory[this.memoryPointer] == 0)
+                        {
+                            int loops = 1;
+
+                            while (loops > 0)
+                            {
+                                this.instructionPointer++;
+
+                                if (this.BF_machine_instructions[this.instructionPointer] == '[')
+                                    loops++;
+                                else if (this.BF_machine_instructions[this.instructionPointer] == ']')
+                                    loops--;
+                            }
+                        }
+                        else
+                            this.loop_call_stack.Push(this.instructionPointer);
+                        goto default;
+
+                    case ']':
+                        this.instructionPointer = this.loop_call_stack.Pop();
+                        break;
+
+                    default:
+                        this.instructionPointer++;
+                        break;
+                }
+            }
+
+            this.Outputs = OutputBuilder.ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ BrainFuck_Interpreter_library_TESTS/Interpreter_BF__TESTS.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using BrainFuck_Interpreter_library;
+
+namespace BrainFuck_Interpreter_library_TESTS
+{
+    [TestClass]
+    public class Interpreter_BF__TESTS
+    {
+        [TestMethod]
+        public void Add_Two_one_digit_numbers_in_BF()
+        {
+            string BF_code = ",>++++++[<-------->-],[<+>-]<.";
+            string Inputs = "35";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+
+            string ActualOutput = interpreter.Outputs;
+            string ExpectedOutput = "8";
+
+            Debug.WriteLine(ActualOutput);
+
+            Assert.AreEqual(ExpectedOutput, ActualOutput, true);
+        }
+
+        [TestMethod]
+        public void Multiply_Two_one_digit_numbers_in_BF()
+        {
+            string BF_code = ",>,>++++++++[<------<------>>-] <<[>[> +> +<< -] >>[<< +>> -] <<< -] >>> ++++++[< ++++++++> -] <.";
+            string Inputs = "23";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+
+            string ActualOutput = interpreter.Outputs;
+            string ExpectedOutput = "6";
+
+            Debug.WriteLine(ActualOutput);
+
+            Assert.AreEqual(ExpectedOutput, ActualOutput, true);
+        }
+
+        [TestMethod]
+        public void Devide_Two_one_digit_numbers_in_BF_andIgnoreComments()
+        {
+            string BF_code = @" ,>,>++++++[-<--------<-------->>] przechowuje dwie cyfry w (0) i (1) od obu odejmujemy 48
+                                <<[                               powtarzaj dopóki dzielna jest niezerowa
+                                >[->+>+<<]                        kopiuj dzielnik z (1) do (2) i (3) (1) się zeruje
+                                >[-<<-                            odejmujemy 1 od dzielnej (0) i dzielnika (2) dopóki (2) nie jest 0
+                                [>]>>>[<[>>>-<<<[-]]>>]<<]        jeżeli dzielna jest zerem wyjdź z pętli
+                                >>>+                              dodaj jeden do ilorazu w (5)
+                                <<[-<<+>>]                        kopiuj zapisany dzielnik z (3) do (1)
+                                <<<]                              przesuń wskaźnik na (0) i powtórz pętlę
+                                >[-]>>>>[-<<<<<+>>>>>]            kopiuj iloraz z (5) do (0)
+                                <<<<++++++[-<++++++++>]<.         dodaj 48 i drukuj wynik";
+            string Inputs = "82";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+
+            string ActualOutput = interpreter.Outputs;
+            string ExpectedOutput = "4";
+
+            Debug.WriteLine(ActualOutput);
+
+            Assert.AreEqual(ExpectedOutput, ActualOutput, true);
+        }
+
+        [TestMethod]
+        public void Read_input_and_print_in_BF()
+        {
+            string BF_code = ">>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.-.<<<<--,<,";
+            string Inputs = "asdefghtjidkoekldmwujfdjncnxoskhowkfndmnowkcxo";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+
+            string ActualOutput = interpreter.Outputs;
+            string ExpectedOutput = "LK";
+
+            Debug.WriteLine(ActualOutput);
+
+            Assert.AreEqual(ExpectedOutput, ActualOutput, true);
+        }
+
+        [TestMethod]
+        public void Print_HelloWorld_in_BF()
+        {
+            string BF_code = "++++++++++ [ > +++++++> ++++++++++> +++> +<<<< - ] > ++. > +. ++++++ +. . ++ +.  > ++.  << +++++++++++++++.  >.  ++ +.  ------.  --------.  > +.  >.";
+            string Inputs = "";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+
+            string ActualOutput = interpreter.Outputs;
+            string ExpectedOutput = "Hello World!\n";
+
+            Debug.WriteLine(ActualOutput);
+
+            Assert.AreEqual(ExpectedOutput, ActualOutput, true);
+        }
+
+        [TestMethod]
+        public void Print_HelloWorld_in_BF_andIgnoreComments()
+        {
+            string BF_code = @"
+                                ++++++++++
+                                [
+                                >+++++++>++++++++++>+++>+<<<<-
+                                ]                       Na początek ustawiamy kilka przydatnych później wartości
+                                >++.                    drukuje 'H'
+                                > +.                    drukuje 'e'
+                                ++++++ +.               drukuje 'l'
+                                .                       drukuje 'l'
+                                ++ +.                   drukuje 'o'
+                                > ++.                   spacja
+                                << +++++++++++++++.     drukuje 'W'
+                                >.                      drukuje 'o'
+                                ++ +.                   drukuje 'r'
+                                ------.                 drukuje 'l'
+                                --------.               drukuje 'd'
+                                > +.                    drukuje '!'
+                                >.  ";
+            string Inputs = "";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+
+            string ActualOutput = interpreter.Outputs;
+            string ExpectedOutput = "Hello World!\n";
+
+            Debug.WriteLine(ActualOutput);
+
+            Assert.AreEqual(ExpectedOutput, ActualOutput, true);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NoInputProvidedWhenNeeded()
+        {
+            string BF_code = ">>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.-.<<<<--,<,";
+            string Inputs = "";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void MissingSomeInput()
+        {
+            string BF_code = ">>++>,>+++++,++,+++++++---++++.-.<<<<--,<,>>,<<,.";
+            string Inputs = "abcde";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void MissingClosingBracket()
+        {
+            string BF_code = "<<<[>>>>";
+            string Inputs = "";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void MissingOpeningBracket()
+        {
+            string BF_code = ",>++++++[<-------->-],<+>-]<.";
+            string Inputs = "35";
+
+            Interpreter_BF interpreter = new Interpreter_BF(BF_code, Inputs);
+            interpreter.Interpret();
+        }
+
+
+    }
+}
